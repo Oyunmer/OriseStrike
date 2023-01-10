25b25f0dc1bfb46b1ebf85073f351f9cb1718e78
ByOyunmer <94802754+Oyunmer@users.noreply.github.com>
2023-01-10 18:10:54 +0300

    Create Camera.java

---
+++ src/physics/Camera.java
@@ -0,0 +1,11 @@
+package physics;
+
+import renderer.math.Vec3;
+
+public class Camera {
+  public static final Vec3 DA = new Vec3();
+  
+  public static final Vec3 DB = new Vec3();
+  
+  public static final Vec3 DISTANCE = new Vec3(0.0D, -50.0D, -500.0D);
+}
