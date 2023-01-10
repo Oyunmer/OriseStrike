244065a268c036c448294a9e5b2be924e50311fc
ByOyunmer <94802754+Oyunmer@users.noreply.github.com>
2023-01-10 18:03:49 +0300

    Create Sounds.java

---
+++ src/audio/Sounds.java
@@ -0,0 +1,9 @@
+package audio;
+
+public class Sounds {
+  public static final Sound WALKING = new Sound("walking.wav");
+  
+  public static final Sound RELOAD = new Sound("reload.wav");
+  
+  public static final Sound SHOOT = new Sound("shoot.wav");
+}
